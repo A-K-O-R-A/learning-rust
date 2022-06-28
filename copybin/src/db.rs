0e89ae4b3b4558a7b23e2f26e9c65180da514f80
AKORA <dragoncatm@gmail.com>
2022-06-28 21:35:45 +0200

    uwu

--- copybin/src/db.rs
+++ copybin/src/db.rs
@@ -1,3 +1,30 @@
-use mongodb::{options::ClientOptions, Client};
+use mongodb::{Client, Database};
+use std::env;
+use std::error::Error;
 
-    pub fn qwq(a: i32) {}
+pub async fn init() -> Result<(Client, Database), Box<dyn Error>> {
+    // Parse a connection string into an options struct.
+    //let client_options = ClientOptions::parse(env::var("MONGO_URI").expect("MONG_URI")).await?;
+
+    // Manually set an option.
+    //client_options.app_name = Some("My App".to_string());
+
+    // Get a handle to the deployment.
+    //let client = Client::with_options(client_options)?;
+    let client = Client::with_uri_str(env::var("MONGO_URI").expect("MONG_URI")).await?;
+
+    // List the names of the databases in that deployment.
+    //for db_name in client.list_database_names(None, None).await? {
+    //    println!("{}", db_name);
+    //}
+
+    // Get a handle to a database.
+    let db = client.database("copybin");
+
+    // List the names of the collections in that database.
+    for collection_name in db.list_collection_names(None).await? {
+        println!("{}", collection_name);
+    }
+
+    Ok((client, db))
+}
--- copybin/src/main.rs
+++ copybin/src/main.rs
@@ -2,24 +2,35 @@
 extern crate rocket;
 extern crate dotenv;
 
+use rocket_db_pools::mongodb;
+use rocket_db_pools::{Connection, Database};
+
 mod db;
 use dotenv::dotenv;
-use std::env;
+
+#[derive(Database)]
+#[database("bins")]
+struct Bins(mongodb::Client);
 
 #[launch]
-fn rocket() -> _ {
+async fn rocket() -> _ {
     dotenv().ok();
 
-    for (key, value) in env::vars() {
-        // println!("{}: {}", key, value);
-    }
+    let (_client, _db) = db::init().await.expect("MongoDB");
 
-    db::qwq();
-
-    rocket::build().mount("/", ![index])
+    rocket::build()
+        .attach(Bins::init())
+        .mount("/", routes![index, owo])
 }
 
 #[get("/")]
 fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/owo")]
+fn owo(mut db: Connection<Bins>) -> &'static str {
+    let db = db.database("copybin");
+
     "Hello, world!"
 }
